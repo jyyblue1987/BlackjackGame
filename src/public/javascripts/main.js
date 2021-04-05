4b304f8f68490ef45675d2f5c4c71a86e8124e1e
jyyblue <jyyblue1987@outlook.com>
2021-04-06 02:17:26 +0800

    [add] show form

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -4,6 +4,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 function main() {
     console.log("main");
+    initEvents();
 }
 
 console.log("Start");
+
+function initEvents() {
+    document.querySelector('.playBtn').addEventListener('click', function(e) {
+        console.log("playBtn");
+        e.preventDefault();
+        startGame();
+    });
+}
+
+function startGame() {
+    // hide form
+    document.querySelector('form').style.display = 'none';
+
+    var startValues = document.querySelector('#startValues').value;
+
+    // get card list by spliting
+    var card_list = startValues.split(",");
+    console.log(card_list);
+}
